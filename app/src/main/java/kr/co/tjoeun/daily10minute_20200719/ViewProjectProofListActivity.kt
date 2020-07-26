2b05053137e38b47ad411bc8d8beafce71a885e3
황선혜 <ghkdtjs6451@gmail.com>
2020-07-26 11:10:38 +0900

    인증 확인 날짜의 기본값을 오늘로 설정

--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/ViewProjectProofListActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/ViewProjectProofListActivity.kt
@@ -2,8 +2,15 @@ package kr.co.tjoeun.daily10minute_20200719
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_project_proof_list.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ViewProjectProofListActivity : BaseActivity() {
+
+//
+//
+    val proofDate = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +24,16 @@ class ViewProjectProofListActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        proofDate 에 담긴 일시 (Calendar) 를 => 2020년 6월 9일의 형태로 문구 (String) 출력.
+//        SimpleDateFormat의 기능중 format 사용
+
+        val sdf = SimpleDateFormat("yyyy년 M월 d일")
+
+
+        val proofDateStr = sdf.format(proofDate.time)
+
+        proofDateTxt.text = proofDateStr
 
     }
 

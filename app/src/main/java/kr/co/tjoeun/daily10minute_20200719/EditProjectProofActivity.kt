c90bdb8a0278cdc305e5987a5949706fd018f366
황선혜 <ghkdtjs6451@gmail.com>
2020-08-01 13:46:58 +0900

    프로젝트 인증하게 화면 진입   - 필요 데이터들 전달 / 표시

--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/EditProjectProofActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/EditProjectProofActivity.kt
@@ -1,9 +1,16 @@
 package kr.co.tjoeun.daily10minute_20200719
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_project_proof.*
+import kr.co.tjoeun.daily10minute_20200719.datas.Project
 
 class EditProjectProofActivity : BaseActivity() {
+
+//    어떤 프로젝트에 대해 인증할지 구별하는 id 값
+    var mProjectId = 0
+
+    lateinit var mProject : Project
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_project_proof)
@@ -17,6 +24,11 @@ class EditProjectProofActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        mProjectId = intent.getIntExtra("projectId", 0)
+
+//        프로젝트의 제목은 intent가 넘겨주는 내용을 그대로 표시
+        projectTitleTxt.text = intent.getStringExtra("projectTitle")
 
     }
 }

a7f7c882e0f3a761b59eb7b7178b6495bd28a093
황선혜 <ghkdtjs6451@gmail.com>
2020-08-02 15:53:14 +0900

    푸시알림 수신 체그 -> 디바이스 토큰 추출, 알림수신시 로그 출력까지만

---
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/fcm/MyFcmService.kt
@@ -0,0 +1,21 @@
+package kr.co.tjoeun.daily10minute_20200719.fcm
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFcmService : FirebaseMessagingService() {
+
+//     앱이 화면에 나와있을 때 푸시알림을 받으면 실행되는 코드
+//     앱이 꺼져있는 상태면 => 자동으로 푸시알림이 뜬다.
+
+    override fun onMessageReceived(p0: RemoteMessage?) {
+        super.onMessageReceived(p0)
+
+        Log.d("푸시알림 수신", p0?.notification?.title)
+
+    }
+}

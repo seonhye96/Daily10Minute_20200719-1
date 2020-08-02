f85742a3ccdbca4d06bbaa2e128eb1c90fd9094c
황선혜 <ghkdtjs6451@gmail.com>
2020-08-02 12:20:17 +0900

    알림데이터 + 표시 xml 추가  - 사용자 프사를 보여줄 공간도 고려   + 내가 한부분 수정 전

--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/NotificationListActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/NotificationListActivity.kt
@@ -2,6 +2,8 @@ package kr.co.tjoeun.daily10minute_20200719
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.tjoeun.daily10minute_20200719.utils.ServerUtil
+import org.json.JSONObject
 
 class NotificationListActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,11 @@ class NotificationListActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        ServerUtil.getRequestProjectNotificationList(mContext, object : ServerUtil.JsonResponseHandler{
+            override fun onResponse(json: JSONObject) {
 
+            }
+
+        })
     }
 }

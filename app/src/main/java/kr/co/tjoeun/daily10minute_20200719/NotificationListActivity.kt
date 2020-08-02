695b8f04b602429c35c44bae5a97981b8d552d4c
황선혜 <ghkdtjs6451@gmail.com>
2020-08-02 13:47:13 +0900

    알림 아이콘 위에 안읽은 알림 갯수 텍스트뷰 추가  - 실 알림 갯수 반영X

--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/NotificationListActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/NotificationListActivity.kt
@@ -2,10 +2,17 @@ package kr.co.tjoeun.daily10minute_20200719
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_notification_list.*
+import kr.co.tjoeun.daily10minute_20200719.adapters.NotificationAdapter
+import kr.co.tjoeun.daily10minute_20200719.datas.Notification
 import kr.co.tjoeun.daily10minute_20200719.utils.ServerUtil
 import org.json.JSONObject
 
 class NotificationListActivity : BaseActivity() {
+
+    val mNotiList = ArrayList<Notification>()
+    lateinit var mNotiAdapter : NotificationAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification_list)
@@ -18,9 +25,26 @@ class NotificationListActivity : BaseActivity() {
     }
 
     override fun setValues() {
-        ServerUtil.getRequestProjectNotificationList(mContext, object : ServerUtil.JsonResponseHandler{
-            override fun onResponse(json: JSONObject) {
 
+        mNotiAdapter = NotificationAdapter(mContext, R.layout.notification_list_item, mNotiList)
+        notificationListView.adapter = mNotiAdapter
+
+        getNotificationListFromServer()
+    }
+
+    fun getNotificationListFromServer(){
+        ServerUtil.getRequestNotificationList(mContext, object : ServerUtil.JsonResponseHandler{
+            override fun onResponse(json: JSONObject) {
+                val data = json.getJSONObject("data")
+                val notifications = data.getJSONArray("notifications")
+
+                for(i in 0 until notifications.length()){
+//                    배열 내부의 JSONArray 추출 => Notification 형태로 변환 => 알림목록에 추가
+                    mNotiList.add(Notification.getNotificationFromJson(notifications.getJSONObject(i)))
+                }
+
+//
+                mNotiAdapter.notifyDataSetChanged()
             }
 
         })
---
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/adapters/NotificationAdapter.kt
@@ -0,0 +1,46 @@
+package kr.co.tjoeun.daily10minute_20200719.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import kr.co.tjoeun.daily10minute_20200719.R
+import kr.co.tjoeun.daily10minute_20200719.datas.Notification
+import kr.co.tjoeun.daily10minute_20200719.datas.Project
+import kr.co.tjoeun.daily10minute_20200719.utils.TimeUtil
+
+class NotificationAdapter(
+    val mContext:Context,
+    resId:Int,
+    val mList:List<Notification>) : ArrayAdapter<Notification>(mContext, resId, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.notification_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        val userProfileImg = row.findViewById<ImageView>(R.id.userProfileImg)
+        val notificationTxt = row.findViewById<TextView>(R.id.notificationTxt)
+        val notitimeTxt = row.findViewById<TextView>(R.id.notitimeTxt)
+
+        val data = mList[position]
+
+        notificationTxt.text = data.title
+        notitimeTxt.text = TimeUtil.getTimeAgoStringFromCalendar(data.createdAt)
+
+        Glide.with(mContext).load(data.actUser.profileImageList[0].imageUrl).into(userProfileImg)
+
+        return row
+    }
+
+
+}
